c57c4329a8457ec7fb12d74f1577742dcbb4a653
praful <prafulpatnecha@gmail.com>
2025-10-27 22:38:11 +0530

    27-10-2025

--- day3/day3_CalculatorApp.py
+++ day3/day3_CalculatorApp.py
@@ -1,23 +1,28 @@
 
-num1 = float(input("Enter Any One Number A : "))
-num2 = float(input("Enter Any One Number B : "))
+# Simple Calculator
 
-print("Select operation:")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
+while True :
+    num1 = float(input("Enter Any One Number A : "))
+    num2 = float(input("Enter Any One Number B : "))
 
-num3 = input("Enter 1 to 4 Any One Number Type : ")
-# while True :
-if num3 == "1" :
-    print("Result:",num1 + num2)
-elif num3 == "2" :
-    print("Result:",num1 - num2)
-elif num3 == "3" :
-    print("Result:",num1 * num2)
-elif num3 == "4" :
-    print("Result:",num1 / num2)
-else:
-    print("Invalid choice!")
-        # break
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("→→→Enter another number to return to the calculator.←←←")
+
+    num3 = input("Enter 1 to 4 Any One Number Type : ")
+    if num3 == "1" :
+        print("Result:",num1 + num2)
+    elif num3 == "2" :
+        print("Result:",num1 - num2)
+    elif num3 == "3" :
+        print("Result:",num1 * num2)
+    elif num3 == "4" :
+        if num1==1 and num2==0:
+            print("Try Again!")
+        else:
+            print("Result:",num1 / num2)
+    else:
+        break
---
+++ day3/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+loat(input("Enter Any One Number A : "))
+    num2 = float(input("Enter Any One Number B : "))
+
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    num3 = i
